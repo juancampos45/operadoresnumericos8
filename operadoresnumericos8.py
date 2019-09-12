b8ff6f6bc4fe9533ebedb1b9a0e4bf894a280238
juancampos45 <55142610+juancampos45@users.noreply.github.com>
2019-09-12 10:42:16 -0400

    Add files via upload

---
+++ operadoresnumericos8.py
@@ -0,0 +1,22 @@
+# Proposito: Trabajar con los operadores aritmrtico
+input('Suponga que a=49 b=5 c=3, pulse cualquier tecla para realizar el siguiente programa: ')
+input('A//(b % 2), pulse cualquier tecla para obtener su resultado')
+# Entrada de datos
+
+a = 49
+
+b = 5
+
+c = 3
+
+# Procedimiento
+
+z = a // (c % 2)
+ 
+# Salida del programa
+
+print(' El resultado del programa es: ',z)
+
+# Fin del programa
+
+input(' pulse cualquier tecla para continuar...')# pausa
